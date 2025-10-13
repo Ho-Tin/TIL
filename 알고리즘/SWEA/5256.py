912a0332f29554568dcf8f3dc8916c787ba5fd7e
Ho-Tin <tmdgh9612@gmail.com>
2025-10-13 19:35:12 +0900

    Create 5256.py

---
+++ 알고리즘/SWEA/5256.py
@@ -0,0 +1,11 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    dp = [0] * (N + 1)
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 3] + dp[i - 2] * 2 + dp[i - 1]
+ 
+    print(f"#{tc} {dp[N]}")
